a980e90aa5ce675169ac3866be3d549bc2e67225
Manuel Montero <87241159+ManuelMC21@users.noreply.github.com>
2025-01-24 22:53:22 +0100

    Find Player in Items Added

--- Assets/Resources/BackEnd/Scripts/Core/Items/DamageBoosterItem.cs
+++ Assets/Resources/BackEnd/Scripts/Core/Items/DamageBoosterItem.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimplePotionItem : MonoBehaviour
+public class DamageBoosterItem : MonoBehaviour
 {
-    public string effect = "Este item recupera 5 puntos de vida";
+    public string effect = "Este item duplica tu daño de ataue en el siguiente ataque";
+
     private Player player;
 
-    private int recovery = 5;
-
+    private int damageBooster(int playerDamage)
+    {
+        return playerDamage * 2;
+    }
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-        player.recoveryHealth(recovery);
+        player.updateDamage(damageBooster(player.damage));   
     }
 
     // Update is called once per frame
