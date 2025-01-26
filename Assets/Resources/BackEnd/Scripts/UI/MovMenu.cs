16e258509bc69dc38cb8bae8947b973b45b15f3e
AndyClemente <andyclmentegago@gmail.com>
2025-01-26 01:13:15 -0500

    menu principal
    menu con dialogos

---
+++ Assets/Resources/BackEnd/Scripts/UI/MovMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovMenu : MonoBehaviour
+{
+    
+    private Transform PlayerTransform;
+    private float MoveX, MoveY;
+    public Animator animator;
+    public float SpeedMove = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerTransform = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+    }
+
+  private void Update()
+    {
+
+            MoveX = Input.GetAxis("Horizontal") * SpeedMove * Time.deltaTime;
+            MoveY = Input.GetAxis("Vertical") * SpeedMove * Time.deltaTime;
+            GetComponent<CapsuleCollider2D>().enabled = true;
+
+            if (MoveY != 0 || MoveX != 0)
+                animator.SetBool("inMove", true);
+            else
+                animator.SetBool("inMove", false);
+
+
+            if (MoveX < 0)
+                transform.localScale = new Vector3(-5, 5, 1);
+            else if (MoveX > 0)
+                transform.localScale = new Vector3(5, 5, 1);
+
+            Vector2 newPosition = PlayerTransform.position + new Vector3(MoveX, MoveY, 0);
+            PlayerTransform.position = newPosition;
+        
+    }
+
+}
